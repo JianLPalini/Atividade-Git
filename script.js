c18dd3918806ba7f8b8f7a1b038966b28a4ded6b
JianLPalini <jianpalini11@gmail.com>
2026-02-18 15:27:06 -0300

    Teste

---
+++ script.js
@@ -0,0 +1,8 @@
+const nome = "Patolino da Silva"
+const idade = 4000
+
+let nome2 = "Perna Longa Curta Perna"
+let idade2 = 5
+
+console.log("Olá!", "Meu nome é ", nome2, "e eu tenho", idade2, "anos")
+// Olá" Meu nome é Jian e eu tenho 27 anos
